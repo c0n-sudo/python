b2f2cd274cf5ecbecab33c66553701ad9c15989d
Miguel H. <mihoffmann@protonmail.com>
2025-09-30 09:15:04 +0200

    newfiles

---
+++ textgenerator.py
@@ -0,0 +1,10 @@
+# START
+name = "Miguel"
+country = "Germany"
+age = 28
+result = None
+
+result = "Hello my name is " + name + " and I am " + str(age) + " years of age and I come from " + country
+print(result)
+# END
+
