cd2aef4d0b171356321e9e37253d2fe39645ce4c
Miguel H. <mihoffmann@protonmail.com>
2025-10-07 11:53:56 +0200

    newfile

---
+++ restaurantCallBack.py
@@ -0,0 +1,16 @@
+#it is a callback example, customer in restaurant put order and waiter deliver food
+
+def checkFoodExistance(food_name:str):
+    #             0        1         2          .........n
+    arr_foods=["Pizza", "Pasta", "Döner"]
+    arr_length = arr_foods.count
+    print(f"we have {arr_length} foods in our menu")
+
+def orderFood(food_name:str, checkFoodExistance):
+    print(f"your {food_name} deliver soon!")
+    checkFoodExistance(food_name)
+
+
+    #--actual program use upper defined methods --#
+    order_string = input("what is your order?")
+    orderFood(order_string,checkFoodExistance)
